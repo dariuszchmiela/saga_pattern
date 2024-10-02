026fb95c59e5ccf2595e10655f5f6173b9d46515
Dariusz Chmiela <dariusz.chmiela@ipfdigital.com>
2024-10-02 06:28:20 +0200

    Initial commit

---
+++ src/test/java/com/dch/saga_pattern/SagaPatternApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dch.saga_pattern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SagaPatternApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
